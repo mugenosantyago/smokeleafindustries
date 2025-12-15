b164b9e63a1130756ba58921822e464ac5b34344
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2025-12-14 18:52:19 -0600

    j

--- src/main/java/net/micaxs/smokeleaf/block/entity/ModBlockEntities.java
+++ src/main/java/net/micaxs/smokeleaf/block/entity/ModBlockEntities.java
@@ -9,6 +9,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModBlockEntities {
@@ -16,53 +17,45 @@ public class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, SmokeleafIndustries.MODID);
 
 
-    // BlockEntityType.Builder API removed in 1.21.8
-    // TODO: Fix BlockEntityType creation API for 1.21.8 - Builder pattern was removed
     public static final Supplier<BlockEntityType<BaseWeedCropBlockEntity>> BASE_WEED_CROP_BE = BLOCK_ENTITIES.register("base_weed_crop_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix BlockEntityType creation"); });
+            () -> new BlockEntityType<>(BaseWeedCropBlockEntity::new, Set.of(ModBlocks.HEMP_CROP.get()), null));
 
 
-    // BlockEntityType.Builder API removed in 1.21.8
-    // TODO: Fix BlockEntityType creation API for 1.21.8
     public static final Supplier<BlockEntityType<GeneratorBlockEntity>> GENERATOR_BE = BLOCK_ENTITIES.register("generator_be",
-            () -> {
-                throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix BlockEntityType creation");
-            });
+            () -> new BlockEntityType<>(GeneratorBlockEntity::new, Set.of(ModBlocks.GENERATOR.get()), null));
 
-    // BlockEntityType.Builder API removed in 1.21.8
-    // TODO: Fix BlockEntityType creation API for 1.21.8
     public static final Supplier<BlockEntityType<GrinderBlockEntity>> GRINDER_BE = BLOCK_ENTITIES.register("grinder_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(GrinderBlockEntity::new, Set.of(ModBlocks.GRINDER.get()), null));
 
     public static final Supplier<BlockEntityType<ExtractorBlockEntity>> EXTRACTOR_BE = BLOCK_ENTITIES.register("extractor_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(ExtractorBlockEntity::new, Set.of(ModBlocks.EXTRACTOR.get()), null));
 
     public static final Supplier<BlockEntityType<LiquifierBlockEntity>> LIQUIFIER_BE = BLOCK_ENTITIES.register("liquifier_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(LiquifierBlockEntity::new, Set.of(ModBlocks.LIQUIFIER.get()), null));
 
     public static final Supplier<BlockEntityType<MutatorBlockEntity>> MUTATOR_BE = BLOCK_ENTITIES.register("mutator_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(MutatorBlockEntity::new, Set.of(ModBlocks.MUTATOR.get()), null));
 
     public static final Supplier<BlockEntityType<SynthesizerBlockEntity>> SYNTHESIZER_BE = BLOCK_ENTITIES.register("synthesizer_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(SynthesizerBlockEntity::new, Set.of(ModBlocks.SYNTHESIZER.get()), null));
 
     public static final Supplier<BlockEntityType<SequencerBlockEntity>> SEQUENCER_BE = BLOCK_ENTITIES.register("sequencer_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(SequencerBlockEntity::new, Set.of(ModBlocks.SEQUENCER.get()), null));
 
     public static final Supplier<BlockEntityType<DryerBlockEntity>> DRYER_BE = BLOCK_ENTITIES.register("dryer_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(DryerBlockEntity::new, Set.of(ModBlocks.DRYER.get()), null));
 
     public static final Supplier<BlockEntityType<DryingRackBlockEntity>> DRYING_RACK_BE = BLOCK_ENTITIES.register("drying_rack_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(DryingRackBlockEntity::new, Set.of(ModBlocks.DRYING_RACK.get()), null));
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ReflectorBlockEntity>> REFLECTOR = BLOCK_ENTITIES.register("reflector_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>(ReflectorBlockEntity::new, Set.of(ModBlocks.REFLECTOR.get()), null));
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<GrowLightBlockEntity>> GROW_LIGHT = BLOCK_ENTITIES.register("grow_light_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>((GrowLightBlockEntity::new, ModBlocks.LED_LIGHT.get()).build());
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<GrowPotBlockEntity>> GROW_POT = BLOCK_ENTITIES.register("grow_pot_be",
-            () -> { throw new UnsupportedOperationException("BlockEntityType.Builder API removed in 1.21.8 - TODO: Fix"); });
+            () -> new BlockEntityType<>((GrowPotBlockEntity::new, ModBlocks.GROW_POT.get()).build());
 
 
 
