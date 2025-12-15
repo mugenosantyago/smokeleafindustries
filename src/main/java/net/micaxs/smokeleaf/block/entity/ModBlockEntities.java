10aa317d48985fbeb36830c344ed103247f90822
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2025-12-14 19:29:24 -0600

    Update ModBlockEntities.java

--- src/main/java/net/micaxs/smokeleaf/block/entity/ModBlockEntities.java
+++ src/main/java/net/micaxs/smokeleaf/block/entity/ModBlockEntities.java
@@ -5,6 +5,7 @@ import net.micaxs.smokeleaf.block.ModBlocks;
 import net.micaxs.smokeleaf.block.custom.SequencerBlock;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
