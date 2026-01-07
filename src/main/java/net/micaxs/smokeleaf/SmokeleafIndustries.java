c1a3ad5ee185c1ca00677beb30b72684197a44ff
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-01-07 10:03:51 -0600

    Update SmokeleafIndustries.java

--- src/main/java/net/micaxs/smokeleaf/SmokeleafIndustries.java
+++ src/main/java/net/micaxs/smokeleaf/SmokeleafIndustries.java
@@ -25,10 +25,14 @@ import com.mojang.logging.LogUtils;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
